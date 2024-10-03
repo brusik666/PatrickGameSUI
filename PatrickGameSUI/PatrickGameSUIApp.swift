e70f75135c8204890f57e5efb61a563945173b48
Brusik <dmytrobrusov@gmail.com>
2024-10-03 00:45:40 -0700

    Initial Commit

---
+++ PatrickGameSUI/PatrickGameSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatrickGameSUIApp.swift
+//  PatrickGameSUI
+//
+//  Created by Brusik on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PatrickGameSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
