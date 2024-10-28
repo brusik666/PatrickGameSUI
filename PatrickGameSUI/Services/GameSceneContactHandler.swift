4ff1893a8a4752258dfc2fe1062baef9ed3957af
Brusik <dmytrobrusov@gmail.com>
2024-10-27 17:26:14 -0700

    jump logic for player implemented

---
+++ PatrickGameSUI/Services/GameSceneContactHandler.swift
@@ -0,0 +1,8 @@
+//
+//  GameSceneContactHandler.swift
+//  PatrickGameSUI
+//
+//  Created by Brusik on 10/26/24.
+//
+
+import Foundation
---
+++ PatrickGameSUI/Services/GameSceneTouchHandler.swift
@@ -0,0 +1,8 @@
+//
+//  GameSceneTouchHandler.swift
+//  PatrickGameSUI
+//
+//  Created by Brusik on 10/26/24.
+//
+
+import Foundation
